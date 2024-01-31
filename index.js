dd01822c3294954ddeee1084f0a8aaa61164056f
Matteo Piccini <gnappo.1@live.it>
2024-01-30 16:49:41 -0800

    end of walkthrough code

---
+++ index.js
@@ -0,0 +1,34 @@
+function receivesAFunction(fn){
+    //! The following line hardcodes the fn rather than using the argument provided
+    // function fn() {
+    // }
+    fn(); //! fn will run its own function's body
+    return fn();  //! fn will run its own function's body; whatever the function body returns, receivesAFunction WILL ALSO return
+}
+
+console.log(receivesAFunction(parseInt))
+// const receivesAFunctionArrow = fn => {
+//     //! The following line hardcodes the fn rather than using the argument provided
+//     // function fn() {
+
+//     // }
+//     fn(); //! fn will run its own function's body
+//     return fn();  //! fn will run its own function's body; whatever the function body returns, receivesAFunction WILL ALSO return
+// }
+
+// const returnsANamedFunction = () => {
+//     // return returnsANamedFunction
+//     return function xyz(){}
+// }
+function returnsANamedFunction() {
+    // return returnsANamedFunction
+    return function xyz(){}
+}
+
+console.log(returnsANamedFunction())
+
+function returnsAnAnonymousFunction(){
+    return () => {}
+}
+
+console.log(returnsAnAnonymousFunction())
